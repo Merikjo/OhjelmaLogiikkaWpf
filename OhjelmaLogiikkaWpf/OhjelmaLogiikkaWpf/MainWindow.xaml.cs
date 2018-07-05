0c733b93b3fc4db3e9ec816cd4e2d805e887805b
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2018-07-05 07:30:09 +0300

    Muutokset 4.7.2018.

--- OhjelmaLogiikkaWpf/OhjelmaLogiikkaWpf/MainWindow.xaml.cs
+++ OhjelmaLogiikkaWpf/OhjelmaLogiikkaWpf/MainWindow.xaml.cs
@@ -26,6 +26,18 @@ namespace OhjelmaLogiikkaWpf
             myBrowser.Navigate(new Uri("http://www.google.com"));
         }
 
+        private void btnTervehdys_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Heissan," + txtName.Text + "!");
+            txtHei.Text = "Hyvä!!!!";
+        }
+
+        private void btnHelloWorld_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hei, maailma!");
+            MessageBox.Show("Hello World!");
+        }
+
         private void btnViikonpaiva_Click(object sender, RoutedEventArgs e)
         {
             DateTime tänään = DateTime.Today;
@@ -80,11 +92,21 @@ namespace OhjelmaLogiikkaWpf
         {
             int i = 123;
             string s = "123";
-            VertaileLukuJaMerkkijono(i, s);
+            VertaileLukuJaMerkkijono(i, s); //tehty if - else -lausekkeesta rutiini VertaileLukuJaMerkkijono
+            //Edit > Refactor > Extract Method
 
             i = 234;
             s = "234";
             VertaileLukuJaMerkkijono(i, s);
+            //Refactoroitu rutiiniksi:
+            //if (i == int.Parse(s))
+            //{
+            //    MessageBox.Show("Ovat samoja!");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Eivät ole samoja!");
+            //}
 
             i = 345;
             s = "345";
@@ -106,9 +128,54 @@ namespace OhjelmaLogiikkaWpf
             }
         }
 
+        //2. Ref -esimerkki
+        private void btnVertaileLukuJaMerkkijonoRef_Click(object sender, RoutedEventArgs e)
+        {
+            //käytetään metodia (Edit > Refactor > Extract Method) = 
+            //luodaan valitusta lohkosta valmis rutiini > annetaan rutiinille nimi:
+            //VertaileLukuJaMerkkijono(i, s);
+
+                //int = value (arvo)
+                //määritetään int i referenssinä > parametristä tulee viittaustyyppi (reference) = (ref i, s)
+                //VertaileLukuJaMerkkijono rutiinille kerrotaan, että luku on alkuperäisessä merkkijonossa
+                int i = 123;
+                string s = "123";
+                VertaileLukuJaMerkkijonoRef(ref i, s);
+
+                i = 234;
+                s = "234";
+                VertaileLukuJaMerkkijonoRef(ref i, s);
+
+                i = 345;
+                s = "345";
+                VertaileLukuJaMerkkijonoRef(ref i, s);
+
+                i = 456;
+                s = "666";
+                VertaileLukuJaMerkkijonoRef(ref i, s);
+        }
+
+            //uusi rutiinimuodostuu>
+            private static void VertaileLukuJaMerkkijonoRef(ref int i, string s)
+            {
+                if (i == int.Parse(s))
+                {
+                    MessageBox.Show("Ovat samoja!");
+                }
+                else
+                {
+                    MessageBox.Show("Eivät ole samoja!");
+                }
+                //lisää yhden luvun
+                i = i + 1;
+            }
+        
+
         private void btnLoop_Click(object sender, RoutedEventArgs e)
         {
             string s = "A";
+
+            //kirjoita for ja paina 2 kertaa tab:
             // for-silmukka (engl. for loop)
             for (int i = 0; i < 1000; i++)
             {
@@ -116,31 +183,23 @@ namespace OhjelmaLogiikkaWpf
             }
             MessageBox.Show(s);
 
+            //kirjoita do ja paina 2 kertaa tab:
             // do-while -silmukka
             do
             {
                 MessageBox.Show("Hiphei!");
+                //toteutuu niin kauan kuin ehto toteutuu
+                //jos ei ole ehtoa, tulee loputon silmukka
             }
             while (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday);
         }
+
 
         private void btnLuku_Click(object sender, RoutedEventArgs e)
         {
             sbyte b = sbyte.Parse(txtLuku.Text);
             b = (sbyte)(b + 1);
             MessageBox.Show("B:n arvo = " + b);
-        }
-
-        private void btnTervehdys_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Heissan," + txtName.Text + "!");
-            txtHei.Text = "Hyvä!!!!";
-        }
-
-        private void btnHelloWorld_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Hei, maailma!");
-            MessageBox.Show("Hello World!");
         }
 
         private void btnDoWhile_Click(object sender, RoutedEventArgs e)
@@ -161,9 +220,43 @@ namespace OhjelmaLogiikkaWpf
             while (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday);
         }
 
+        private void btnFor_Click(object sender, RoutedEventArgs e)
+        {
+            string s = "B";
+
+            for (int i = 10; i < 500; i++)
+
+            {
+                s = s + "B";
+            }
+            MessageBox.Show(s);
+            //muodostuu 491 B:tä
+        }
+
+
+        //Navigointi:
         private void btnDefaultSite_Click(object sender, RoutedEventArgs e)
         {
             myBrowser.Navigate(new Uri("http://www.google.com"));
         }
+
+        private void btnNumerotyyppi_Click(object sender, RoutedEventArgs e)
+
+        //unsigned integer byte = ei voi olla negatiivinen luku
+        {
+            byte b = byte.Parse(txtByte.Text);//Parse = käännetään txtboxin luku merkkijonoksi
+            b = (byte)(b + 1);
+            MessageBox.Show("B:n arvo = " + b);
+        }
+
+        private void btnSignedByte_Click(object sender, RoutedEventArgs e)
+        //signed integer sbyte = luvut -128 to 127
+        {
+            sbyte b = sbyte.Parse(txtSbyte.Text);
+            b = (sbyte)(b + 1);
+            MessageBox.Show("B:n arvo = " + b);
+        }
+
+
     }
 }
